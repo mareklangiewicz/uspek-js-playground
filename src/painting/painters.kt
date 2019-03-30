9c4722a9e848fa5353c8cbb28b42a244256fdf8d
Marek Langiewicz <marek.langiewicz@gmail.com>
2019-03-30 11:40:42 +0100

    Refactor random utils

--- src/painting/painters.kt
+++ src/painting/painters.kt
@@ -1,42 +1,42 @@
 package painting
 
-import kotlinx.html.currentTimeMillis
-import org.w3c.dom.CanvasRenderingContext2D
+import org.w3c.dom.CanvasRenderingContext2D as Ctx
 import org.w3c.dom.HTMLCanvasElement
 import org.w3c.dom.get
 import kotlin.browser.document
-import kotlin.browser.window
 import kotlin.random.Random
 
 const val WIDTH = 800
 const val HEIGHT = 800
 
-val context by lazy {
+private val ctx by lazy {
     val canvas = document.getElementsByTagName("canvas")[0] as HTMLCanvasElement
     canvas.width  = WIDTH
     canvas.height = HEIGHT
-    canvas.getContext("2d") as CanvasRenderingContext2D
+    canvas.getContext("2d") as Ctx
 }
 
-val random = Random(currentTimeMillis())
+infix fun Int.randomTo(to: Int) = (this .. to).random()
+infix fun Int.randomUntil(until: Int) = (this until until).random()
 
-fun around(value: Int) = value + random.nextInt(value / 3) - value / 6 + 1
-fun around(value: Double) = value + random.nextDouble(value / 3) - value / 6 + 1
+infix fun Double.randomUntil(to: Double) = Random.nextDouble(this, to)
 
-fun paintSomething() = context.run {
+fun around(value: Int) = value - value / 6 randomTo value + value / 6
+fun around(value: Double) = value - value / 6 randomUntil value + value / 6
+
+fun paintSomething() = ctx.run {
 //    fillStyle = "rgb(200, 0, 0, 0.2)"
 //    fillRect(around(10.0), around(10.0), around(50.0), around(50.0))
 //    fillStyle = "rgb(0, 0, 200, 0.3)"
 //    fillRect(around(30.0), around(30.0), around(50.0), around(50.0))
-
     paintCurve()
 }
 
-fun paintClear() = context.clearRect(0.0, 0.0, WIDTH.toDouble(), HEIGHT.toDouble())
+fun paintClear() = ctx.clearRect(0.0, 0.0, WIDTH.toDouble(), HEIGHT.toDouble())
 
 private var hue = 0
 
-private fun CanvasRenderingContext2D.paintCurve() {
+private fun Ctx.paintCurve() {
     beginPath()
     lineWidth = around(20.0)
     moveTo(around(300.0), around(300.0))
