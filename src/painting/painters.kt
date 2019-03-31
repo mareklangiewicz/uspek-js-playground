b6471e04ea1ee9cce4d7d9e325cdcb9910693c7d
Marek Langiewicz <marek.langiewicz@gmail.com>
2019-03-31 14:44:47 +0200

    Mimic Wassily Kandinsky

--- src/painting/painters.kt
+++ src/painting/painters.kt
@@ -14,27 +14,43 @@ fun paintSomething() = Kandinsky.paint()
 
 interface Painter {
     val name: String
-    fun paint()
+    fun paintBack()
+    fun paintFront()
+    fun paint() { paintBack(); paintBack(); paintFront(); paintFront() }
 }
 
 object Kandinsky : Painter {
     override val name = "Wassily Kandinsky"
-    override fun paint() = ctx.run {
-        randomPainting() // TODO
+    override fun paintBack() = ctx.run {
+        repeat(1 rnd 4) { randomPolyline(1 rnd 3, 200, 300, opacity = 10 rnd 80) }
+    }
+    override fun paintFront() = ctx.run {
+        repeat(1 rnd 4) { randomCurve(6 rnd 10, 20 rnd (20 rnd 100)) }
+        repeat(2 rnd 8) { randomCurve(2 rnd 8, 10 rnd (10 rnd 100)) }
+        repeat(1 rnd 3) { randomCircle(2 rnd (8 rnd 64)) }
+        repeat(1 rnd 4) { randomPolyline(1 rnd 3, 2, 6) }
     }
 }
 
 object Pollock : Painter {
-    override val name = "Jackson Pollock"
-    override fun paint() {
+    override fun paintBack() {
         TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
     }
+
+    override fun paintFront() {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override val name = "Jackson Pollock"
 
 }
 
 object Picasso : Painter {
     override val name = "Pablo Picasso"
-    override fun paint() {
+    override fun paintBack() {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+    override fun paintFront() {
         TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
     }
 }
@@ -78,11 +94,18 @@ private fun Ctx.randomCurve(minWidth: Int, maxWidth: Int = minWidth, minHue: Int
     strokeStyle = "hsl(${minHue rnd maxHue}, 60%, 50%)"
 }
 
-private fun Ctx.randomPolyline(segments: Int = 1, minWidth: Int = 4, maxWidth: Int = minWidth, minHue: Int = 0, maxHue: Int = 360) = strokePath {
+private fun Ctx.randomPolyline(
+    segments: Int = 1,
+    minWidth: Int = 4,
+    maxWidth: Int = minWidth,
+    minHue: Int = 0,
+    maxHue: Int = 360,
+    opacity: Int = 100
+) = strokePath {
     moveTo(rndx, rndy)
     repeat(segments) { lineTo(rndx, rndy) }
     lineWidth = (minWidth rnd maxWidth).toDouble()
-    strokeStyle = "hsl(${minHue rnd maxHue}, 60%, 50%)"
+    strokeStyle = "hsl(${minHue rnd maxHue}, 60%, 50%, $opacity%)"
 }
 
 private inline fun Ctx.strokePath(block: Ctx.() -> Unit) {
