d851d7315a7232d93c0a4ba988922dd6c3200b5b
Marek Langiewicz <marek.langiewicz@gmail.com>
2019-03-31 13:35:47 +0200

    Improve painting util functions; add randomCircle util fun

--- src/painting/painters.kt
+++ src/painting/painters.kt
@@ -1,22 +1,23 @@
 package painting
 
-import org.w3c.dom.CanvasRenderingContext2D as Ctx
 import org.w3c.dom.HTMLCanvasElement
 import org.w3c.dom.get
 import kotlin.browser.document
+import kotlin.math.PI
 import kotlin.random.Random
+import org.w3c.dom.CanvasRenderingContext2D as Ctx
 
 const val WIDTH = 800.0
 const val HEIGHT = 800.0
 
 private val ctx by lazy {
     val canvas = document.getElementsByTagName("canvas")[0] as HTMLCanvasElement
-    canvas.width  = WIDTH.toInt()
+    canvas.width = WIDTH.toInt()
     canvas.height = HEIGHT.toInt()
     canvas.getContext("2d") as Ctx
 }
 
-infix fun Int.rnd(to: Int) = (this .. to).random()
+infix fun Int.rnd(to: Int) = Random.nextInt(this, to + 1)
 infix fun Double.rnd(to: Double) = Random.nextDouble(this, to)
 
 val Int.near get() = this - this / 6 rnd this + this / 6
@@ -24,19 +25,31 @@ val Double.near get() = this - this / 6 rnd this + this / 6
 
 private val rndx get() = 0.0 rnd WIDTH
 private val rndy get() = 0.0 rnd HEIGHT
-private val rndhue get() = 0 rnd 360
 
 fun paintSomething() = ctx.run {
-    repeat(10) { randomCurve() }
+    repeat(4) { randomCurve(100, 140) }
+    repeat(10) { randomCurve(1, 30) }
+    repeat(4) { randomCircle(4, 10) }
 }
 
 fun clearCanvas() = ctx.clearRect(0.0, 0.0, WIDTH, HEIGHT)
 
-private fun Ctx.randomCurve() {
-    beginPath()
-    lineWidth = 1.0 rnd 15.0
+private fun Ctx.randomCircle(minWidth: Int, maxWidth: Int = minWidth, minHue: Int = 0, maxHue: Int = 360) = strokePath {
+    val radius = 40.0 rnd (100.0 rnd 300.0)
+    arc(rndx, rndy, radius, 0.0, 2 * PI)
+    lineWidth = (minWidth rnd maxWidth).toDouble()
+    strokeStyle = "hsl(${minHue rnd maxHue}, 60%, 50%)"
+}
+
+private fun Ctx.randomCurve(minWidth: Int, maxWidth: Int = minWidth, minHue: Int = 0, maxHue: Int = 360) = strokePath {
     moveTo(rndx, rndy)
     bezierCurveTo(rndx, rndy, rndx, rndy, rndx, rndy)
-    strokeStyle = "hsl($rndhue, 50%, 50%)"
+    lineWidth = (minWidth rnd maxWidth).toDouble()
+    strokeStyle = "hsl(${minHue rnd maxHue}, 60%, 50%)"
+}
+
+private inline fun Ctx.strokePath(block: Ctx.() -> Unit) {
+    beginPath()
+    block()
     stroke()
 }
